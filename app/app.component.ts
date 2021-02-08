2d88741751f21c18d697299747488fe04821dfe4
Allan Carvalho <allan.carvalho@kk.pt>
2021-02-08 21:38:24 +0000

    ngFOr and iterating collections

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,21 +1,61 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <button (click)="handleClick(username.value)">Change Name</button>
-      <input type="text" #username>
-      <div>{{ name }}</div>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
+          <li>{{ i }}: {{ passenger.fullname }}</li>
+        </template>
+
+      </ul>
+
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">{{ i }}: {{ passenger.fullname }}</li>
+      </ul>
     </div>
   `
 })
 export class AppComponent {
 
-  name: string;
+  passengers: Passenger[] = [
+    {
+      id: 1,
+      fullname: 'Stephen',
+      checkedIn: true
+    },
+    {
+      id: 2,
+      fullname: 'Rose',
+      checkedIn: false
+    },
+    {
+      id: 3,
+      fullname: 'James',
+      checkedIn: true
+    },
+    {
+      id: 4,
+      fullname: 'Louise',
+      checkedIn: true
+    },
+    {
+      id: 5,
+      fullname: 'Tina',
+      checkedIn: false
+    },
+  ]
 
-  handleClick(value: string) {
-    this.name = value;
-  }
+
+
 }
