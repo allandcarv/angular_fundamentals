9809cfe79e9fd7cab195a1cd441ac251e70dbd10
Allan Carvalho <allan.carvalho@kk.pt>
2021-02-08 16:23:53 +0000

    Root Module with @NgModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
+    BrowserModule,
+    CommonModule
   ],
   declarations: [
+    AppComponent
+  ],
+  bootstrap: [
     AppComponent
   ]
 })
