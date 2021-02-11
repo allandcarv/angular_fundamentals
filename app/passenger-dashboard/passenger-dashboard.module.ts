9c39f6562bd8e8ccaecba61f3972546530cb6b38
Allan Carvalho <allan.carvalho@kk.pt>
2021-02-11 17:37:10 +0000

    Understanding @Injectable

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,6 +1,6 @@
-import { fromPromise } from "rxjs/observable/fromPromise";
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpModule } from '@angular/http';
 
 // containers
 import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
@@ -18,7 +18,10 @@ import { PassengerDashboardService } from './passenger-dashboard.service';
     PassengerCountComponent,
     PassengerDetailComponent
   ],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HttpModule
+  ],
   exports: [PassengerDashboardComponent],
   providers: [ PassengerDashboardService ]
 })
